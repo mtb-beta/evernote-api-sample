011f23c3c3b94794f1ca60bfae7fa6ff7e6cd933
mtb_beta <mtb.toya0403@gmail.com>
2018-11-20 01:57:10 +0900

    ノートブックのリストを得る

---
+++ sample/sample2_select_notebook.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+from evernote.api.client import EvernoteClient
+import evernote.edam.type.ttypes as Types
+
+
+load_dotenv()
+TOKEN = os.getenv("EVERNOTE_DEVELOPPER_TOKEN")
+
+def main():
+    client = EvernoteClient(token=TOKEN)
+    note_store = client.get_note_store()
+    notebooks = note_store.listNotebooks()
+    for notebook in notebooks:
+        print("id:{}, name:{}".format(notebook.guid, notebook.name))
+
+if __name__ == "__main__":
+    main()
