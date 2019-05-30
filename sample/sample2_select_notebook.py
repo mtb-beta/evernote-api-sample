42d36cad505b259d9267b91a8cbcfd59a762389a
Tatsuya Matoba <mtb.toya0403@gmail.com>
2019-05-31 02:09:07 +0900

    refactor sample2

--- sample/sample2_select_notebook.py
+++ sample/sample2_select_notebook.py
@@ -11,12 +11,28 @@ from evernote.api.client import EvernoteClient
 TOKEN = config("EVERNOTE_DEVELOPPER_TOKEN")
 USE_SANDBOX = config("USE_SANDBOX", True, cast=bool)
 
+class EvernoteApp:
+    def __init__(self, token, use_sandbox):
+        self.token = token
+        self.use_sandbox = use_sandbox
+        self.client = EvernoteClient(token=self.token, sandbox=self.use_sandbox)
+        self.note_store = self.client.get_note_store()
+
+    def create_notebook(self, name):
+        notebook = Types.Notebook()
+        notebook.name = name
+        self.note_store.createNotebook(notebook)
+
+    @property
+    def all_notebooks(self):
+        return self.note_store.listNotebooks()
+
+
 def main():
-    client = EvernoteClient(token=TOKEN, sandbox=USE_SANDBOX)
-    note_store = client.get_note_store()
-    notebooks = note_store.listNotebooks()
-    for notebook in notebooks:
+    evernote_app = EvernoteApp(token=TOKEN, use_sandbox=USE_SANDBOX)
+    for notebook in evernote_app.all_notebooks:
         print("id:{}, name:{}".format(notebook.guid, notebook.name))
+
 
 if __name__ == "__main__":
     main()
