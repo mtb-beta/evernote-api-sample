9eddf58e5fd555087f3e2553c942d1305d67db66
Tatsuya Matoba <mtb.toya0403@gmail.com>
2019-05-31 02:05:20 +0900

    refactor sample1

--- sample/sample1_make_notebook.py
+++ sample/sample1_make_notebook.py
@@ -12,14 +12,21 @@ import evernote.edam.type.ttypes as Types
 TOKEN = config("EVERNOTE_DEVELOPPER_TOKEN")
 USE_SANDBOX = config("USE_SANDBOX", True, cast=bool)
 
+class EvernoteApp:
+    def __init__(self, token, use_sandbox):
+        self.token = token
+        self.use_sandbox = use_sandbox
+        self.client = EvernoteClient(token=self.token, sandbox=self.use_sandbox)
+        self.note_store = self.client.get_note_store()
+
+    def create_notebook(self, name):
+        notebook = Types.Notebook()
+        notebook.name = name
+        self.note_store.createNotebook(notebook)
 
 def main():
-    notebook = Types.Notebook()
-    notebook.name = "My Sample Notebook"
-
-    client = EvernoteClient(token=TOKEN, sandbox=USE_SANDBOX)
-    note_store = client.get_note_store()
-    note_store.createNotebook(notebook)
+    evernote_app = EvernoteApp(token=TOKEN, use_sandbox=USE_SANDBOX)
+    evernote_app.create_notebook("My Sample Notebook")
 
 if __name__ == "__main__":
     main()
