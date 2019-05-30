c59a7a0ef33ba0827cf95c992e235cf8eb9dcef3
Tatsuya Matoba <mtb.toya0403@gmail.com>
2019-05-31 02:27:31 +0900

    refactor sample3

--- sample/sample3_create_note.py
+++ sample/sample3_create_note.py
@@ -12,25 +12,49 @@ TOKEN = config("EVERNOTE_DEVELOPPER_TOKEN")
 USE_SANDBOX = config("USE_SANDBOX", True, cast=bool)
 NOTEBOOK = "My Sample Notebook"
 
-def main():
-    client = EvernoteClient(token=TOKEN, sandbox=USE_SANDBOX)
-    note_store = client.get_note_store()
-    notebooks = note_store.listNotebooks()
-    target_notebook = None
-    for notebook in notebooks:
-        if notebook.name == NOTEBOOK:
-            target_notebook = notebook
-            break
 
-    if not notebook:
+class EvernoteApp:
+    def __init__(self, token, use_sandbox):
+        self.token = token
+        self.use_sandbox = use_sandbox
+        self.client = EvernoteClient(token=self.token, sandbox=self.use_sandbox)
+        self.note_store = self.client.get_note_store()
+
+    def create_notebook(self, name):
+        notebook = Types.Notebook()
+        notebook.name = name
+        self.note_store.createNotebook(notebook)
+
+    @property
+    def all_notebooks(self):
+        return self.note_store.listNotebooks()
+
+    def filter_notebook(self, keyword):
+        notebooks = []
+        for notebook in self.all_notebooks:
+            if keyword in notebook.name:
+                notebooks.append(notebook)
+        return notebooks
+
+    def create_note(self, notebook, title, content):
+        note = Types.Note()
+        note.title = title
+        note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
+        note.content += '<en-note>{}</en-note>'.format(content)
+        note.notebookGuid = notebook.guid
+        return self.note_store.createNote(note)
+
+def main():
+    evernote_app = EvernoteApp(token=TOKEN, use_sandbox=USE_SANDBOX)
+    notebooks = evernote_app.filter_notebook(NOTEBOOK)
+    if not notebooks:
         print("Notebook {} Not Found.".format(NOTEBOOK))
 
-    note = Types.Note()
-    note.title = "This is my first note"
-    note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
-    note.content += '<en-note>Hello, world!</en-note>'
-    note.notebookGuid = target_notebook.guid
-    note = note_store.createNote(note)
+    evernote_app.create_note(
+        notebook=notebooks[0],
+        title="This is my first note",
+        content="Hello, world!"
+    )
 
 if __name__ == "__main__":
     main()
