f169b284efee2bfc7e2e5b899a4d42f3502bb3bd
Tatsuya Matoba <mtb.toya0403@gmail.com>
2019-05-31 19:26:27 +0900

    refactor sample4

--- sample/sample4_list_from_notebook.py
+++ sample/sample4_list_from_notebook.py
@@ -15,45 +15,86 @@ TOKEN = config("EVERNOTE_DEVELOPPER_TOKEN")
 USE_SANDBOX = config("USE_SANDBOX", True, cast=bool)
 NOTEBOOK = "My Sample Notebook"
 
-def main():
-    client = EvernoteClient(token=TOKEN, sandbox=USE_SANDBOX)
-    note_store = client.get_note_store()
 
-    # ノートブック一覧を取得し、目当てのノートブックを探す
-    notebooks = note_store.listNotebooks()
-    target_notebook = None
-    for notebook in notebooks:
-        if notebook.name == NOTEBOOK:
-            target_notebook = notebook
-            break
+class Note:
+    def __init__(self, app, note_meta):
+        self.app = app
+        self.note_meta = note_meta
 
-    if not target_notebook:
-        print("Notebook {} Not Found.".format(NOTEBOOK))
+    @property
+    def title(self):
+        return self.note_meta.title
 
-    # 検索条件を指定。ノートブックのIDを指定。更新日でソート
-    note_filter = NoteFilter()
-    note_filter.notebookGuid = target_notebook.guid
-    note_filter.order = NoteSortOrder.UPDATED
-
-    # 検索結果のフォーマットを指定。タイトルを含む
-    result_format = NotesMetadataResultSpec()
-    result_format.includeTitle = True
-
-    # メタデータリストを取得
-    note_meta_list = note_store.findNotesMetadata(note_filter, 0, 10, result_format)
-    
-    # メタデータリストからノートそのものを取得
-    for note_meta in note_meta_list.notes:
-        print(note_meta.title)
+    @property
+    def content(self):
         # ノートIDからノートを取得
-        note = note_store.getNote(note_meta.guid, True, True, True, True)
+        note = self.app.note_store.getNote(self.note_meta.guid, True, True, True, True)
 
         # ノートに含まれているXMLをパース
         content_soup = BeautifulSoup(note.content, 'html.parser')
-        content = content_soup.find('en-note').text
-        print(content)
+        return content_soup.find('en-note').text
 
+class EvernoteApp:
+    def __init__(self, token, use_sandbox):
+        self.token = token
+        self.use_sandbox = use_sandbox
+        self.client = EvernoteClient(token=self.token, sandbox=self.use_sandbox)
+        self.note_store = self.client.get_note_store()
 
+    def create_notebook(self, name):
+        notebook = Types.Notebook()
+        notebook.name = name
+        self.note_store.createNotebook(notebook)
+
+    @property
+    def all_notebooks(self):
+        return self.note_store.listNotebooks()
+
+    def filter_notebook(self, keyword):
+        notebooks = []
+        for notebook in self.all_notebooks:
+            if keyword in notebook.name:
+                notebooks.append(notebook)
+        return notebooks
+
+    def create_note(self, notebook, title, content):
+        note = Types.Note()
+        note.title = title
+        note.content = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'
+        note.content += '<en-note>{}</en-note>'.format(content)
+        note.notebookGuid = notebook.guid
+        return self.note_store.createNote(note)
+
+    def list_notes(self, notebook):
+        # ノート検索条件を指定。ノートブックのIDを指定。更新日でソート
+        note_filter = NoteFilter()
+        note_filter.notebookGuid = notebook.guid
+        note_filter.order = NoteSortOrder.UPDATED
+
+        # ノート検索結果のフォーマットを指定。タイトルを含む
+        result_format = NotesMetadataResultSpec()
+        result_format.includeTitle = True
+
+        # ノートのメタデータリストを取得
+        note_meta_list = self.note_store.findNotesMetadata(note_filter, 0, 10, result_format)
+        return [Note(self, note_meta) for note_meta in note_meta_list.notes]
+
+def main():
+    evernote_app = EvernoteApp(token=TOKEN, use_sandbox=USE_SANDBOX)
+    
+    # 目当てのノートを探す
+    notebooks = evernote_app.filter_notebook(NOTEBOOK)
+    if not notebooks:
+        print("Notebook {} Not Found.".format(NOTEBOOK))
+    target_notebook = notebooks[0]
+
+    # 目当てのノートブックに含まれるノートのメタ情報を取得
+    notes = evernote_app.list_notes(target_notebook)
+
+    # メタデータリストからノートそのものを取得
+    for note in notes:
+        print(note.title)
+        print(note.content)
 
 
 if __name__ == "__main__":
